af2fa58fb4594844ea3a5bb39fdb3bc632053423
IRFAN <irfanchowdhury80@gmail.com>
2020-02-02 14:58:11 +0600

    Single File Component

--- src/App.vue
+++ src/App.vue
@@ -1,63 +1,36 @@
 <template>
   <div id="app">
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-      <a class="navbar-brand" href="#">Super Store</a>
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-
-      <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <form class="form-inline my-2 my-lg-0">
-          <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-          <button class="btn btn-outline-info my-2 my-sm-0" type="submit">Search</button>
-        </form>
-      </div>
-    </nav>
+    
+    <Navbar></Navbar>
 
     <div class="container-fluid p-3">
       <div class="row">
+
         <div class="col-sm-9">
-          <div class="row">
-              <div class="card m-2" style="width: 18rem;">
-                <img src="https://image3.mouthshut.com/images/imagesp/925039961s.jpg" class="card-img-top" alt="...">
-                <div class="card-body">
-                  <h5 class="card-title">Card title</h5>
-                  <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                  <a href="#" class="btn btn-primary">Go somewhere</a>
-                </div>
-            </div>
-              <div class="card m-2" style="width: 18rem;">
-                <img src="https://5.imimg.com/data5/AC/GM/LF/SELLER-51536152/lux-rose-shoap-500x500.jpg" class="card-img-top" alt="...">
-                <div class="card-body">
-                  <h5 class="card-title">Card title</h5>
-                  <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                  <a href="#" class="btn btn-primary">Go somewhere</a>
-                </div>
-            </div>
-              <div class="card m-2" style="width: 18rem;">
-                <img src="https://lh3.googleusercontent.com/proxy/26EuPAMWKZDkVBiYz7RLUci4B_sPUjTOm1Ter5Da_qaWkcF1inms0fxq4NwsxwPxa_O1L0PLxdJglA86Hus8csV4VAlJs_Fc_UoTvUl9oTo1fpnYN0gl_3qd27xJH4_l7GfrKc9ckRpNnEA2kwwH4v67fFSB" class="card-img-top" alt="...">
-                <div class="card-body">
-                  <h5 class="card-title">Card title</h5>
-                  <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                  <a href="#" class="btn btn-primary">Go somewhere</a>
-                </div>
-            </div>
-          </div>
+          <Inventory></Inventory>
         </div>
+
         <div class="col-sm-3">
-          <ul class="list-group">
-            <li class="list-group-item">Item - Price</li>
-            <li class="list-group-item">Item - Price</li>
-            <li class="list-group-item">Item - Price</li>
-          </ul>
+            <Cart></Cart>
         </div>
+      
       </div>
     </div>
   </div>
 </template>
 
 <script>
-export default {}
+import Navbar from './components/Navbar'
+import Cart from './components/Cart'
+import Inventory from './components/Inventory'
+
+export default {
+  components:{
+    Navbar,
+    Cart,
+    Inventory 
+  }
+}
 </script>
 
 <style>
---
+++ src/components/Inventory.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="row">
+        <div class="card m-2" style="width: 18rem;">
+            <img src="https://image3.mouthshut.com/images/imagesp/925039961s.jpg" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+        </div>
+        <div class="card m-2" style="width: 18rem;">
+            <img src="https://5.imimg.com/data5/AC/GM/LF/SELLER-51536152/lux-rose-shoap-500x500.jpg" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+        </div>
+        <div class="card m-2" style="width: 18rem;">
+            <img src="https://lh3.googleusercontent.com/proxy/26EuPAMWKZDkVBiYz7RLUci4B_sPUjTOm1Ter5Da_qaWkcF1inms0fxq4NwsxwPxa_O1L0PLxdJglA86Hus8csV4VAlJs_Fc_UoTvUl9oTo1fpnYN0gl_3qd27xJH4_l7GfrKc9ckRpNnEA2kwwH4v67fFSB" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style>
+</style>
